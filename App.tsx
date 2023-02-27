b3c0fc61c41372ef79074bb4f974d8336203ebd9
Emilien Vaschalde <emi@macbook-air-de-emilien.home>
2023-02-27 08:25:48 +0100

    First commit - with routing

--- App.tsx
+++ App.tsx
@@ -1,20 +1,58 @@
+//react
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+//compo
+import Trainings from './screens/Trainings/Trainings';
+import Musiques from './screens/Musiques/Musiques';
+import Profile from './screens/Profile/Profile';
+//icons
+import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+
+const tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar />
+      <tab.Navigator
+                screenOptions={({route}) => ({
+                    tabBarIcon: ({focused, color, size}) => { // les icones
+                      let iconName;
+                
+                      if (route.name == "Entrainements") {
+                        return <AntDesign name="profile" size={24} color={focused ? "orange" : "white"} />
+                      } else if (route.name == "Musiques") {
+                        return <FontAwesome name="music" size={24} color={focused ? "orange" : "white"} />
+                      }else if (route.name == "Mon plan") {
+                        return <FontAwesome name="user" size={24} color={focused ? "orange" : "white"} />
+                      }
+                    },
+                    tabBarActiveTintColor: 'orange', //text
+                    tabBarInactiveTintColor: 'white',
+                    tabBarLabelStyle: {
+                      fontSize: 13
+                    },
+                    
+                    tabBarStyle: { // style additionnel
+                        height : 90,
+                        paddingVertical: 7,
+                        backgroundColor: "black",
+                        position: 'absolute',
+                        borderTopWidth: 0,
+                        paddingBottom:30
+                    },
+    
+                    headerShown: true //show or not header
+                })}
+          
+        >
+              <tab.Screen name="Entrainements" component={Trainings}/>
+              <tab.Screen name="Musiques" component={Musiques} />
+              <tab.Screen name="Mon plan" component={Profile} />
+        </tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/Trainings/Trainings.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Trainings = () => {
+    return (
+       <View>
+            <Text>Traingings</Text>
+       </View>
+    );
+};
+
+export default Trainings;
